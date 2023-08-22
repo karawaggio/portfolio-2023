03b02ad20ad4fc2ff0ce968172a569df870fc3ca
Sergey <sergey@lugaro.com>
2023-08-21 21:13:45 -0700

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <section>
+        <Head>
+          <title>Sergey Karavaev Portfolio</title>
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        </Head>
+
+        <main>
+        
+        </main>
+    </section>
+  )
+}
