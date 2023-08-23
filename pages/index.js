3619cb075102c0658c3a23e84aa880778199a2cb
Sergey <sergey@lugaro.com>
2023-08-22 18:25:45 -0700

    add footer

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <section>
+    <section className="font-titilliumweb">
         <Head>
           <title>Sergey Karavaev | Portfolio</title>
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         </Head>
 
-        <main className="font-titilliumweb">
+        <main>
 
         </main>
+        
+        <footer>
+        
+        </footer>
     </section>
   )
 }
