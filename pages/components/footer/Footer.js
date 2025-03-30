839c4fb9c83bf12be0e036085e4bd7a8c39536e8
Sergey <sergeykaravaev44@gmail.com>
2025-03-29 23:50:25 -0700

    Add copyright to the footer

--- pages/components/footer/Footer.js
+++ pages/components/footer/Footer.js
@@ -1,10 +1,12 @@
 import MobileNav from "./MobileNav"
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return(
         <footer className="xs-max:mt-20">
             <section className="xs-max:hidden xs-min:h-24 bg-accent text-dark flex justify-center items-center xs-min:px-8">
-              <div id="copyright"></div>
+              <div id="copyright" class="text-psocialmd">&copy; {currentYear} Sergey Karavaev</div>
             </section>
 
             <section className="fixed bottom-0 right-0 left-0 z-10 pt-2 bg-accent xs-min:hidden xs-max:h-16"> {/* class="mob-foot-nav-wrap fixed-bottom" */}
