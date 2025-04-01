38456d86d9ab0f1b8ad6593db7b97b11113f9a5a
Sergey <sergeykaravaev44@gmail.com>
2025-04-01 10:56:33 -0700

    added two class by mistake to jsx, changed to className

--- pages/components/footer/Footer.js
+++ pages/components/footer/Footer.js
@@ -6,10 +6,10 @@ function Footer() {
     return(
         <footer className="xs-max:mt-20">
             <section className="xs-max:hidden xs-min:h-24 bg-accent text-dark flex justify-center items-center xs-min:px-8">
-              <div id="copyright" class="text-psocialmd">&copy; {currentYear} Sergey Karavaev</div>
+              <div id="copyright" className="text-psocialmd">&copy; {currentYear} Sergey Karavaev</div>
             </section>
 
-            <section className="fixed bottom-0 right-0 left-0 z-10 pt-2 bg-accent xs-min:hidden xs-max:h-16"> {/* class="mob-foot-nav-wrap fixed-bottom" */}
+            <section className="fixed bottom-0 right-0 left-0 z-10 pt-2 bg-accent xs-min:hidden xs-max:h-16"> {/* className="mob-foot-nav-wrap fixed-bottom" */}
               <MobileNav/>
             </section>
         </footer>
